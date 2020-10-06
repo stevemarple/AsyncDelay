40376ba1f0a6dede272df093181fd1d5697267f6
Steve Marple <s.marple@systemloco.com>
2020-10-06 11:50:20 +0100

    Add example to read A0

---
+++ examples/ReadA0/ReadA0.ino
@@ -0,0 +1,25 @@
+/*
+ * Simple demonstration of AsyncDelay to read the A0 analogue input
+ * every 50 ms. 
+*/
+#include <AsyncDelay.h>
+
+
+AsyncDelay samplingInterval;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  samplingInterval.start(50, AsyncDelay::MILLIS);
+}
+
+
+void loop(void)
+{
+  if (samplingInterval.isExpired()) {
+    uint16_t count  = analogRead(A0);
+    samplingInterval.repeat();
+    Serial.print(count);
+    Serial.print('\n');
+  }
+}
